2fee3974882061ef595e3443f3bdf215a7b69567
Maika <maika.tp.0331@gmail.com>
2022-06-13 18:10:51 +0900

    この前と同じようなエラーが出てしまいました。

--- src/views/TriviaView.vue
+++ src/views/TriviaView.vue
@@ -29,23 +29,99 @@ export default {
       imagepath: require("@/views/images/latte.jpg"),
 
       feedback: "",
-      quizs: { [
-       {
-          text: "エスプレッソをミルクで１対９で割った飲み物は？",
-         Image: "@/views/images/cafe.jpg",
-         choices: [
+      quizlist: {
+        quiz1: [
           {
-            text: "カフェオレ",
-            isCorrect: false,
-            feedback:
-              "残念！カフェオレはドリップコーヒーとミルクが５対５で割れた飲み物です！",
+            text: "エスプレッソをミルクで１対９で割った飲み物は？",
+            Image: "@/views/images/cafe.jpg",
+            choices: [
+              {
+                text: "カフェオレ",
+                isCorrect: false,
+                feedback:
+                  "残念！カフェオレはドリップコーヒーとミルクが５対５で割れた飲み物です！",
+              },
+              {
+                text: "カフェラテ",
+                isCorrect: true,
+                feedback:
+                  "正解！スターバックスでカフェラテを頼むときは「スターバックスラテ」！",
+              },
+              {
+                text: "カフェモカ",
+                isCorrect: false,
+                feedback:
+                  "カフェモカはエスプレッソ・ミルク・チョコレートシロップの割合が、2: 7:1の飲み物！",
+              },
+              {
+                text: "カプチーノ",
+                isCorrect: false,
+                feedback:
+                  "カプチーノはエスプレッソと温めたミルクとフォームミルクが1:1:1の飲み物！",
+              },
+            ],
+          },
+        ],
+        quiz2: [
+          {
+            text: "スタバにタンブラーを持参すると何円引きになる？",
+            Image: "@/views/images/tumbler.jpg",
+            choices: [
+              {
+                text: "10円",
+                isCorrect: false,
+                feedback: "残念！",
+              },
+              {
+                text: "20円",
+                isCorrect: true,
+                feedback: "正解！環境にもいいね！",
+              },
+              {
+                text: "30円",
+                isCorrect: false,
+                feedback: "残念！",
+              },
+              {
+                text: "40円",
+                isCorrect: false,
+                feedback: "残念！",
+              },
+            ],
+          },
+        ],
+        quiz3:[
+          {
+            text:"スタバのドリンクで２番目に大きいサイズは？",
+            Image:"@/views/images/stb.size.jpg",
+            choices: [
+              {
+                text:"トール",
+                isCorrect: false,
+                feedback: "残念!トールは350㎖!下から２番目のサイズ！",
+              },
+              {
+                text:"ベンティー",
+                isCorrect: false,
+                feedback: "おしい!ベンティーは590㎖!1番大きいサイズ!",
+              },
+              {
+                text:"ショート",
+                isCorrect: false,
+                feedback: "残念!ショートは240㎖!１番小さいサイズ!",
+              },
+              {
+                text:"グランデ",
+                isCorrect: true,
+                feedback: "正解!グランデは470㎖!",
+              },
+            ]
           }
+        ],
 
-        ]
-       }
-       ]
-    },
-    }
+        }
+      },
+    };
   },
   methods: {
     choice(i) {
@@ -55,7 +131,7 @@ export default {
       }
     },
   },
-};
+}
 </script>
 
 <style>
