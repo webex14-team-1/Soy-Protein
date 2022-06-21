860a8cf1f8eb304704d5b6bb7ede45ee0004fe39
Tatsuki-Kurosawa <kuro96831@gmail.com>
2022-06-21 21:19:47 +0900

    warn

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,51 +1,21 @@
 module.exports = {
   root: true,
-
   env: {
     node: true,
   },
-
-  // extends: [
-  //   "plugin:vue/vue3-essential",
-  //   "eslint:recommended",
-  //   "plugin:prettier/recommended",
-  // ],
-
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+  ],
   parserOptions: {
     parser: "@babel/eslint-parser",
   },
-
-  // extends: [
-  //   "plugin:vue/vue3-essential",
-  //   "eslint:recommended",
-  //   "plugin:prettier/recommended",
-  // ],
-  // extends: [
-  //   "plugin:vue/vue3-essential",
-  //   "eslint:recommended",
-  //   "plugin:prettier/recommended",
-  // ],
-  // extends: [
-  //   "plugin:vue/vue3-essential",
-  //   "eslint:recommended",
-  //   "plugin:prettier/recommended",
-  // ],
-  // extends: [
-  //   "plugin:vue/vue3-essential",
-  //   "eslint:recommended",
-  //   "plugin:prettier/recommended",
-  // ],
-  // extends: [
-  //   "plugin:vue/vue3-essential",
-  //   "eslint:recommended",
-  //   "plugin:prettier/recommended",
-  // ],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
-
-  extends: [
+  "prettier/prettier": [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
     "plugin:prettier/recommended",
